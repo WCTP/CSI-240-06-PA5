4bb3c03e86111bdb52a643e2cabb7e9b91ffcaca
WCTP <walter.proulx@mymail.champlain.edu>
2017-04-06 14:18:06 -0400

    Update doctorOperations.cpp

--- doctorOperations.cpp
+++ doctorOperations.cpp
@@ -1,4 +1,5 @@
 #include "doctorOperations.h"
+#include "constants.h"
 
 
 void doctorOperations(Patient **&patients, Doctor doctors[], int numberOfDoctor)
@@ -8,9 +9,9 @@ void doctorOperations(Patient **&patients, Doctor doctors[], int numberOfDoctor)
 
 int getDoctorIndex(Doctor doctors[], int numberOfDoctor, string name)
 {
-	int index;
+	int index = -1;
 
-	for (index = 0; i < numberOfDoctor; i++)
+	for (int i = 0; i < numberOfDoctor && index != i; i++)
 	{
 		if (doctors[i].getName() == name)
 		{
@@ -26,7 +27,7 @@ bool isDoctorExist(Doctor doctors[], int numberOfDoctor, string name)
 {
 	bool doesExist = false;
 
-	for (int i = 0; i < numberOfDoctor && doesExist = false; i++)
+	for (int i = 0; i < numberOfDoctor && doesExist == false; i++)
 	{
 		if (doctors[i].getName() == name)
 		{
@@ -40,12 +41,70 @@ bool isDoctorExist(Doctor doctors[], int numberOfDoctor, string name)
 
 int loadDoctor(Doctor *&doctors)
 {
-	
+	int numDoctors, index, numOfPatients;
+	string name, id;
+	ifstream fin;
 
-	return 0; 
+	fin.open(DOCTORS_FILE);
+
+	if (fin.good())
+	{
+		fin >> numDoctors;
+		fin.ignore();
+		
+		doctors = new Doctor[numDoctors];
+		
+		for (int i = 0; i < numDoctors; i++)
+		{
+			getline(fin, name);
+			doctors[i].setName(name);
+
+			getline(fin, id);
+			doctors[i].setId(id);
+
+			fin >> index;
+			doctors[i].setIndex(index);
+
+			fin >> numOfPatients;
+			doctors[i].setNumberOfPatient(numOfPatients);
+			
+			fin.ignore();
+		}
+	}
+	else
+	{
+		cout << "FILE DOESN'T EXIST";
+	}
+
+	fin.close();
+
+	return numDoctors; 
 }
 
 
 void storeDoctor(Doctor doctors[], int numberOfDoctor)
 {
+	ofstream fout;
+
+	fout.open(DOCTORS_FILE);
+
+	if (fout.good())
+	{
+		fout << numberOfDoctor;
+		
+		for (int i = 0; i < numberOfDoctor; i++)
+		{
+			fout << '\n';
+			fout << doctors[i].getName() << '\n';
+			fout << doctors[i].getId() << '\n';
+			fout << doctors[i].getIndex() << " "
+				<< doctors[i].getNumberOfPatient();
+		}
+	}
+	else
+	{
+		cout << "FILE NOT FOUND";
+	}
+
+	fout.close();
 }
